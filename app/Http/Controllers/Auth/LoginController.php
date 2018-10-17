5a4e81efaa0a7aada31ebb08f7888a0ad8fc103f
darkwoolf10 <pashafudko@ukr.net>
2018-10-17 14:36:13 +0300

    hardcode login and password

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\User;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -36,4 +39,14 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    public function login(Request $request)
+    {
+        if ($request->email = 'admin@gmail.com' && $request->password = 'admin') {
+            $user = User::find($request->email); // you must have record in your user table to trigger the login.
+            Auth::login($user);
+            return redirect('player');
+        }
+    }
+
 }
