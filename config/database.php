8796d7525f52b883a017a8d546fe0fa89b4c0053
darkwoolf10 <woolftreuser@yandex.ru>
2018-10-29 02:02:37 +0200

    add remote db config

--- config/database.php
+++ config/database.php
@@ -43,7 +43,7 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'mrc_root'),
+            'database' => env('DB_DATABASE', 'mrc_db'),
             'username' => env('DB_USERNAME', 'mrc_root'),
             'password' => env('DB_PASSWORD', '12345678'),
             'unix_socket' => env('DB_SOCKET', ''),
